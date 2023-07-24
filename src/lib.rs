90fc4d3761e6fb7656d18dad19edb37c63a031dd
Robert Jördens <rj@quartiq.de>
2023-07-24 13:01:00 +0200

    use tinyvec

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,65 @@
 #![no_std]
-use core::{str, ops};
+use core::{ops, str};
+use tinyvec::SliceVec;
 
-pub struct SliceString<'a> {
-    buf: &'a mut [u8],
-    len: usize,
-}
+pub struct SliceString<'a>(SliceVec<'a, u8>);
 
 impl<'a> SliceString<'a> {
     pub fn new(buf: &'a mut [u8]) -> Self {
-        Self {buf, len: 0}
+        Self(SliceVec::from_slice_len(buf, 0))
     }
 
     pub fn as_str(&self) -> &str {
         // split_at_unchecked?
-        unsafe { str::from_utf8_unchecked(&self.buf[..self.len]) }
+        unsafe { str::from_utf8_unchecked(&self.0) }
     }
 
     pub fn as_mut_str(&mut self) -> &mut str {
         // split_at_unchecked?
-        unsafe { str::from_utf8_unchecked_mut(&mut self.buf[..self.len]) }
+        unsafe { str::from_utf8_unchecked_mut(&mut self.0) }
     }
 
     pub fn capacity(&self) -> usize {
-        self.buf.len()
+        self.0.capacity()
     }
 
     pub fn clear(&mut self) {
-        self.len = 0;
+        self.0.clear();
     }
 
     pub fn push_str(&mut self, string: &str) -> Result<(), ()> {
         let bytes = string.as_bytes();
-        let len = self.len + bytes.len();
-        if self.capacity() < len {
+        if self.capacity() < self.0.len() + bytes.len() {
             return Err(());
         }
-        self.buf[self.len..len].copy_from_slice(bytes);
-        self.len = len;
+        self.0.extend_from_slice(bytes);
         Ok(())
     }
 
     pub fn pop(&mut self) -> Option<char> {
         let ch = self.chars().rev().next()?;
-        self.len -= ch.len_utf8();
+        self.0.truncate(self.len() - ch.len_utf8());
         Some(ch)
     }
 
     pub fn push(&mut self, c: char) -> Result<(), ()> {
         let len = c.len_utf8();
-        if self.capacity() < self.len + len {
-            return Err(())
+        if self.capacity() < self.len() + len {
+            return Err(());
         }
         if len == 1 {
-            self.buf[self.len] = c as u8;
+            self.0.push(c as u8);
         } else {
-            c.encode_utf8(&mut self
-                    .buf[self.len..]);
+            self.0
+                .extend_from_slice(c.encode_utf8(&mut [0; 4]).as_bytes());
         }
-        self.len += len;
         Ok(())
     }
 
-   pub fn truncate(&mut self, new_len: usize) {
-        if new_len <= self.len {
+    pub fn truncate(&mut self, new_len: usize) {
+        if new_len <= self.len() {
             assert!(self.is_char_boundary(new_len));
-            self.len = new_len;
+            self.0.truncate(new_len);
         }
     }
 }
@@ -85,6 +80,7 @@ mod tests {
     fn new() {
         let mut buf = [0u8; 32];
         let mut s = SliceString::new(&mut buf[..]);
+        assert_eq!(0, s.len());
         s.push_str("Hello world!").unwrap();
         assert_eq!(s.as_str(), "Hello world!");
     }
